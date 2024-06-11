0e47356f715c7fcf7aec597a3f88cef2931b5828
Dziamid Zayankouski <dziamid@gmail.com>
2024-06-11 11:06:09 +0400

    refactor func build to bundle local dependencies

---
+++ func/esbuild.ts
@@ -0,0 +1,56 @@
+import esbuild from 'esbuild';
+import { omit } from 'ramda';
+import packageJson from './package.json';
+import { writeFile } from 'fs/promises';
+
+esbuild
+  .build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    platform: 'node',
+    outfile: 'dist/index.js',
+    format: 'esm',
+    external: getExternal(packageJson),
+  })
+  .catch(() => process.exit(1));
+
+await writeFile('dist/package.json', JSON.stringify(getDistPackageJson(packageJson), null, 2));
+
+type PackageJson = {
+  main?: string;
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
+  bundledDependencies?: string[];
+  scripts?: Record<string, string>;
+};
+
+function getExternal(packageJson: PackageJson) {
+  const { dependencies, devDependencies } = getUnbundled(packageJson);
+
+  return [...Object.keys(dependencies), ...Object.keys(devDependencies)];
+}
+
+function getDistPackageJson(packageJson: PackageJson): PackageJson {
+  const { dependencies, devDependencies } = getUnbundled(packageJson);
+
+  return {
+    ...packageJson,
+    main: 'index.js',
+    scripts: {},
+    dependencies,
+    devDependencies: {},
+    bundledDependencies: [],
+  };
+}
+
+function getUnbundled(packageJson: PackageJson) {
+  const { bundledDependencies = [], dependencies = {}, devDependencies = {} } = packageJson;
+
+  const unbundledDependencies = omit(bundledDependencies, dependencies);
+  const unbundledDevDependencies = omit(bundledDependencies, devDependencies);
+
+  return {
+    dependencies: unbundledDependencies,
+    devDependencies: unbundledDevDependencies,
+  };
+}
