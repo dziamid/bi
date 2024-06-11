1f1e9aae0c0dacd63612dec8857d994bec9a6581
Dziamid Zayankouski <dziamid@gmail.com>
2024-06-11 12:38:54 +0400

    refactor: move libararies to packages/ dir, remove lock lock files

--- func/esbuild.ts
+++ func/esbuild.ts
@@ -1,6 +1,7 @@
 import esbuild from 'esbuild';
 import { omit } from 'ramda';
 import packageJson from './package.json';
+import { mkdir, rm, writeFile } from 'fs/promises';
 
 esbuild
   .build({
@@ -13,6 +14,9 @@ esbuild
   })
   .catch(() => process.exit(1));
 
+await rm('dist', { recursive: true, force: true });
+await mkdir('dist');
+await writeFile('dist/package.json', JSON.stringify(getDistPackageJson(packageJson), null, 2));
 
 type PackageJson = {
   main?: string;
@@ -26,6 +30,19 @@ function getExternal(packageJson: PackageJson) {
   const { dependencies, devDependencies } = getUnbundled(packageJson);
 
   return [...Object.keys(dependencies), ...Object.keys(devDependencies)];
+}
+
+function getDistPackageJson(packageJson: PackageJson): PackageJson {
+  const { dependencies, devDependencies } = getUnbundled(packageJson);
+
+  return {
+    ...packageJson,
+    main: 'index.js',
+    scripts: {},
+    dependencies,
+    devDependencies: {},
+    bundledDependencies: [],
+  };
 }
 
 function getUnbundled(packageJson: PackageJson) {
