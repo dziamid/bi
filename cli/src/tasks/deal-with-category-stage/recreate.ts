04c605f79fc03542f2a71a8d3afacfb7ae9b368d
Dziamid Zayankouski <dziamid@gmail.com>
2024-06-16 16:35:55 +0400

    script to create a deal_with_category_stage_view view

---
+++ cli/src/tasks/deal-with-category-stage/recreate.ts
@@ -0,0 +1,32 @@
+import { BigQuery } from '@google-cloud/bigquery'; // Create a BigQuery client
+import { bigqueryV2 } from '@bi/bitrix24';
+
+const datasetId = bigqueryV2.config.datasetId;
+
+// Create a BigQuery client
+const bigquery = new BigQuery();
+
+// SQL query to create the view
+const query = `
+  SELECT
+    s.*,
+    d.*,
+    CONCAT(s.SORT, ' ', s.NAME) as NAME_WITH_ORDER
+  FROM
+    \`${datasetId}.${bigqueryV2.schema.dealCategoryStageTable.name}\` s
+  LEFT JOIN
+    \`${datasetId}.${bigqueryV2.schema.dealTable.name}\` d
+  ON
+    s.STATUS_ID = d.STAGE_ID
+`;
+
+const viewId = `deal_with_category_stage_view`;
+
+await bigqueryV2.table.deleteTable(bigquery, datasetId, viewId);
+
+const [view] = await bigqueryV2.table.createTable(bigquery, datasetId, viewId, {
+  type: 'VIEW',
+  view: query,
+});
+
+console.log(`View ${view.id} created successfully.`);
